99fd08d018164b7905c4b0ce0d151e88916747f8
Vladimir Moiseev neo <lutii@ngs.ru>
2022-05-02 07:49:40 +0700

    first_page

--- samplsite/bboard/urls.py
+++ samplsite/bboard/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import index
+from .views import index, by_rubric
 from .views import vova
+from .views import hh
 
 urlpatterns = [
+	path('<int:rubric_id/>',by_rubric),
 	path('',index),
 	path('v1/',vova),
+	path('hh/',hh),
 ]
--- samplsite/bboard/views.py
+++ samplsite/bboard/views.py
@@ -1,12 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 from .models import Bb
+from .models import Rubric
+
+def by_rubric(request, rubric_id):
+	bbs = Bb.objects.filter(rubric= rubric_id)
+	rubrics = Rubric.objects.all()
+	current_rubric = Rubric.objects.get(pk = rubric_id)
+	context = {'bbs': bbs, 'rubrics':rubrics, 'current_rubric':current_rubric}
+	return render(request, 'bboard/by_rybric.html', context)
+
+# def index(request):
+# 	s = 'list of advertisement \r\n\r\n\r\n'
+# 	for bb in Bb.objects.order_by('-published'):
+# 		s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
+# 	return HttpResponse( s, content_type = 'text/plain; charset=utf-8')
 
 def index(request):
-	s = 'list of advertisement \r\n\r\n\r\n'
-	for bb in Bb.objects.order_by('-published'):
-		s += bb.title + '\r\n' + bb.content + '\r\n\r\n'
-	return HttpResponse( s, content_type = 'text/plain; charset=utf-8')
+	template = loader.get_template('bboard/index.html')
+	bbs = Bb.objects.order_by('-published')
+	context = {'bbs' : bbs }
+	return HttpResponse(template.render(context, request))
+
+def hh(request):
+	bbs = Bb.objects.order_by('-published')
+	return render(request,'bboard/index.html', {'bbs':bbs})
 
 	
 def vova(request):
