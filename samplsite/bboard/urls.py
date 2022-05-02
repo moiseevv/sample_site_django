64ee419e737ab2bb237fe53416de4b4434a2f57c
Vladimir Moiseev neo <lutii@ngs.ru>
2022-05-02 08:18:55 +0700

    second page with controller

--- samplsite/bboard/urls.py
+++ samplsite/bboard/urls.py
@@ -5,7 +5,7 @@ from .views import vova
 from .views import hh
 
 urlpatterns = [
-	path('<int:rubric_id/>',by_rubric),
+	path('<int:rubric_id>/', by_rubric),
 	path('',index),
 	path('v1/',vova),
 	path('hh/',hh),
